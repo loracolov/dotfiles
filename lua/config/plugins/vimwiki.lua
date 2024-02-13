46fa92dc44614ed12f94661aceda426854d7f3e2
Riley Vogel <vogel.riley@gmail.com>
2024-02-13 13:58:37 -0500

    only vault files editable

--- lua/config/plugins/vimwiki.lua
+++ lua/config/plugins/vimwiki.lua
@@ -2,17 +2,13 @@ return {
     "vimwiki/vimwiki",
     -- event = "BufEnter *.md",
     init = function()
+        vim.g.vimwiki_global_ext = 0
         vim.g.vimwiki_list = {
             {
                 path = "~/Documents/Notes/Vault/",
                 syntax = "markdown",
                 ext = ".md",
             },
-        }
-        vim.g.vimwiki_ext2syntax = {
-            [".md"] = "markdown",
-            [".markdown"] = "markdown",
-            [".mdown"] = "markdown",
         }
     end
 }
